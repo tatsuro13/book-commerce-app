f599a03c9d112de4951e4ce5fd83b57462f02e9c
Tatsuro Osuka <oscarwild13@gmail.com>
2024-05-03 10:40:06 +0900

    microCMSからデータ取得したものを表示

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -1,10 +1,42 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
+import { BookType } from "../types/types";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+type BookProps = {
+  book: BookType;
+};
 
 // eslint-disable-next-line react/display-name
-const Book = ({ book }: any) => {
+const Book = ({ book }: BookProps) => {
+  const [showModal, setShowModal] = useState(false);
+  const {data: session} = useSession();
+  const user = session?.user;
+  const router = useRouter();
+
+ const handleCancel = () => {
+    setShowModal(false);
+  }
+
+  const handlePurchaseClick = () => {
+    setShowModal(true);
+  }
+
+  const handlePurchaseConfirm = () => {
+    if(!user) {
+      //ログインぺージに遷移
+      setShowModal(false);
+      router.push('/login');
+    }else{
+      //stripeの決済処理
+      alert('購入しました！');
+      setShowModal(false);
+    }
+  }
+
   return (
     <>
       {/* アニメーションスタイル */}
@@ -25,10 +57,10 @@ const Book = ({ book }: any) => {
       `}</style>
 
       <div className="flex flex-col items-center m-4">
-        <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+        <a onClick={handlePurchaseClick} className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
           <Image
             priority
-            src={book.thumbnail}
+            src={book.thumbnail.url}
             alt={book.title}
             width={250}
             height={350}
@@ -41,17 +73,19 @@ const Book = ({ book }: any) => {
           </div>
         </a>
 
-        {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
+        {showModal && ( 
+          <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
           <div className="bg-white p-8 rounded-lg">
             <h3 className="text-xl mb-4">本を購入しますか？</h3>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
+            <button onClick={handlePurchaseConfirm} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
               購入する
             </button>
-            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+            <button onClick={handleCancel} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
               キャンセル
             </button>
           </div>
-        </div> */}
+          </div>
+        )}
       </div>
     </>
   );
